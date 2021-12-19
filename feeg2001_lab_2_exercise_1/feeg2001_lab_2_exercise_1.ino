71ab5030d083eb1522fe225a8d7ecbbe81ecb678
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-12-19 15:10:36 +0800

    change lab 2 exercise 1

--- feeg2001_lab_2_exercise_1/feeg2001_lab_2_exercise_1.ino
+++ feeg2001_lab_2_exercise_1/feeg2001_lab_2_exercise_1.ino
@@ -2,31 +2,25 @@
 #define trigPin 3
 #define speakerPin 9
 
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
-
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  pinMode(speakerPin, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("Ultrasonic Sensor HC-SR04 Test");
-  Serial.println("with Arduino UNO R3");
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(speakerPin, OUTPUT);
+    Serial.begin(9600);
+    Serial.println("Ultrasonic Sensor HC-SR04 Test");
+    delay(1000);
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  if (distance < 20) {
-    tone(speakerPin, 666, 20);
-  }
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    int distance = duration * 0.034 / 2;
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    (distance < 20) ? tone(speakerPin, 1500, 20) : noTone(speakerPin);
 }
