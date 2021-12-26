85359a47a554a4572173243d71f6ce9cd7116423
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-12-26 18:49:31 +0800

    ledArray

--- ledArray/ledArray.ino
+++ ledArray/ledArray.ino
@@ -1,29 +1,16 @@
-const int pin1 = 5, pin2 = 6, pin3 = 9, pin4 = 10, pin5 = 11;
+const int ledPins[] = {5, 6, 9, 10, 11};
 
 void setup() {
-    pinMode(pin1, OUTPUT);
-    pinMode(pin2, OUTPUT);
-    pinMode(pin3, OUTPUT);
-    pinMode(pin4, OUTPUT);
-    pinMode(pin5, OUTPUT);
+    for (const int pin : ledPins) {pinMode(pin, OUTPUT);}
 }
 
 void loop() {
-    
-}
-
-void blink() {
-    int val = 0;
-    for (int i=0; i<10; i++) {
-        val += 10;
-        analogWrite(pin1, val);
-        delay(200);
+    for (const int pin : ledPins) {
+        digitalWrite(pin, HIGH);
+        delay(50);
     }
-    for (int i=0; i<10; i++) {
-        val -= 10;
-        analogWrite(pin1, val);
-        delay(200);
-        analogWrite(pin2, val);
-        delay(200);
+    for (const int pin : ledPins) {
+        digitalWrite(pin, LOW);
+        delay(50);
     }
 }
