df4f9cb3c94a8cbc047d34b6e34409ef0143a466
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-26 21:54:34 +0800

    Update Lab8_Task2_Light_Logger.ino

--- Lab8_Task2_Light_Logger/Lab8_Task2_Light_Logger.ino
+++ Lab8_Task2_Light_Logger/Lab8_Task2_Light_Logger.ino
@@ -6,7 +6,7 @@
 // A shorter version of the original tutorial from Adafruit
 
 // how many milliseconds between grabbing data and logging it. 1000 ms is once a second
-#define LOG_INTERVAL  1000 // mills between entries (reduce to take more/faster data)
+#define LOG_INTERVAL 1000  // mills between entries (reduce to take more/faster data)
 
 // how many milliseconds before writing the logged data permanently to disk
 // set it to the LOG_INTERVAL to write each time (safest)
@@ -16,11 +16,11 @@
 #define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
 uint32_t syncTime = 0; // time of last sync()
 
-#define ECHO_TO_SERIAL   1 // echo data to serial port
-#define WAIT_TO_START    0 // Wait for serial input in setup()
+#define ECHO_TO_SERIAL 1  // echo data to serial port
+#define WAIT_TO_START  0  // Wait for serial input in setup()
 
 // The analog pins that connect to the sensors
-#define photocellPin 0           // analog 0
+#define photocellPin 0  // analog 0
 
 RTC_DS1307 RTC; // define the Real Time Clock object
 
@@ -30,46 +30,40 @@ const int chipSelect = 10;
 // the logging file
 File logfile;
 
-void error(char *str)
-{
-  Serial.print("error: ");
-  Serial.println(str);
-  while(1);
+void error(char* str) {
+    Serial.print("error: ");
+    Serial.println(str);
+    while(1);
 }
 
-void setup(void)
-{
-  Serial.begin(9600);
-  Serial.println();
-  
+void setup() {
+    Serial.begin(9600);
+    Serial.println();
 #if WAIT_TO_START
-  Serial.println("Type any character to start");
-  while (!Serial.available());
+    Serial.println("Type any character to start");
+    while (!Serial.available());
 #endif //WAIT_TO_START
-
-  // initialize the SD card
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
-  pinMode(10, OUTPUT);
-  
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    error("Card failed, or not present");
-  }
-  Serial.println("card initialized.");
-  
-  // create a new file
-  char filename[] = "LOGGER00.CSV";
-  for (uint8_t i = 0; i < 100; i++) {
+    // initialize the SD card
+    Serial.print("Initializing SD card...");
+    // make sure that the default chip select pin is set to
+    // output, even if you don't use it:
+    pinMode(10, OUTPUT);
+    // see if the card is present and can be initialized:
+    if (!SD.begin(chipSelect)) {
+        error("Card failed, or not present");
+    }
+    Serial.println("card initialized.");
+    // create a new file
+    char filename[] = "LOGGER00.CSV";
+    for (uint8_t i = 0; i < 100; i++) {
     filename[6] = i/10 + '0';
     filename[7] = i%10 + '0';
     if (! SD.exists(filename)) {
-      // only open a new file if it doesn't exist
-      logfile = SD.open(filename, FILE_WRITE); 
-      break;  // leave the loop!
+        // only open a new file if it doesn't exist
+        logfile = SD.open(filename, FILE_WRITE); 
+        break;  // leave the loop!
     }
-  }
+}
   
   if (! logfile) {
     error("couldnt create file");
@@ -96,27 +90,23 @@ void setup(void)
   //analogReference(EXTERNAL);
 }
 
-void loop(void)
-{
-  DateTime now;
-
-  // delay for the amount of time we want between readings
-  delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
-  
-  // log milliseconds since starting
-  uint32_t m = millis();
-  logfile.print(m);           // milliseconds since start
-  logfile.print(", ");    
+void loop() {
+    DateTime now;
+    // delay for the amount of time we want between readings
+    delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
+    // log milliseconds since starting
+    uint32_t m = millis();
+    logfile.print(m);           // milliseconds since start
+    logfile.print(", ");    
 #if ECHO_TO_SERIAL
-  Serial.print(m);         // milliseconds since start
-  Serial.print(", ");  
+    Serial.print(m);         // milliseconds since start
+    Serial.print(", ");  
 #endif
-
-  // fetch the time
-  now = RTC.now();
-  // log time
-  logfile.print(now.unixtime()); // seconds since 1/1/1970
-  logfile.print(", ");
+    // fetch the time
+    now = RTC.now();
+    // log time
+    logfile.print(now.unixtime()); // seconds since 1/1/1970
+    logfile.print(", ");
   logfile.print('"');
   logfile.print(now.year(), DEC);
   logfile.print("/");
@@ -168,7 +158,4 @@ void loop(void)
   if ((millis() - syncTime) < SYNC_INTERVAL) return;
   syncTime = millis();
   logfile.flush();
-
 }
-
-
