e6909f0e91c4b6525502122b017d90e2315e2896
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-12-26 16:50:12 +0800

    update files

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -2,17 +2,16 @@ const int sensorPin = A0, ledPin = 9;
 int sensorValue = 0;
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);  // PWM
     Serial.begin(9600);
+    Serial.println("Potentiometer and LED test");
+    delay(500);
 }
 
 void loop() {
-    sensorValue = analogRead(sensorPin);
+    sensorValue = map(analogRead(sensorPin), 0, 1023, 0, 255);
     Serial.print("Sensor Value: ");
     Serial.println(sensorValue);
-    digitalWrite(ledPin, HIGH);
-    analogWrite(ledPin, sensorValue/7);
-    digitalWrite(ledPin, LOW);
-    analogWrite(ledPin, sensorValue/7);
-    delay(200);
+    analogWrite(ledPin, sensorValue);
+    delay(100);
 }
