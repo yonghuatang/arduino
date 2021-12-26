6c03c0139eae25fcfaa4eae812ef70ae919fd60b
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-26 21:55:49 +0800

    Delete Lab9Task1.ino

--- Lab9Task1/Lab9Task1.ino
+++
@@ -1,9 +0,0 @@
-// Connect VIN to +5V
-// Connect GND to Ground
-// Connect GPS RX (data into GPS) to Digital 0
-// Connect GPS TX (data out from GPS) to Digital 1
-void setup() {
-}
-void loop() {
-}
-
