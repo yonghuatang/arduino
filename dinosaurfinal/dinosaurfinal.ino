2e1fc37ab830cbfef92282c7b982e7097252478b
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-26 19:27:05 +0800

    Update dinosaurfinal.ino

--- dinosaurfinal/dinosaurfinal.ino
+++ dinosaurfinal/dinosaurfinal.ino
@@ -1,16 +1,15 @@
 /*
 University of Southampton Malaysia - FEEG2001 Systems Design and Computing
 Semester 1 Summative Design Assessment - The Arduino Dinosaur
-Arduino Sketch v0.3 - Group 14
-Last modified: 28 November 2021 (Sunday)
-Github: kaizer222 - https://github.com/kaizer222/dino_design_2021
+Arduino Sketch v0.6 - Group 14
+Last modified:  26 December 2021 (Sunday)
 */
 
 #include <Servo.h>
 
-const int ledPin=2, trigPin=3, echoPin=4, buzzerPin=5, ldrPin=A0;
-unsigned int ldrValue, ambientLight, distance, presetDistance = 15, wheelPos = 70;
-Servo wheelServo, jawServo, handServo, legServo;
+const int ledPin=2, trigPin=3, echoPin=4, buzzerPin=5, ldrPin=A1;
+unsigned int ldrValue, ambientLight, distance, presetDistance=15, wheelPos=90, wheelPos_2=90;
+Servo wheelServo, wheelServo_2, jawServo, handServo, legServo;
 
 void setup() {
     Serial.begin(9600);
@@ -20,24 +19,47 @@ void setup() {
     pinMode(buzzerPin, OUTPUT);
     pinMode(ldrPin, INPUT);
     wheelServo.attach(9);
+    wheelServo_2.attach(8);
     jawServo.attach(10);
     handServo.attach(11);
     legServo.attach(12);
-    wheelServo.write(wheelPos);
+    reset_wheel(wheelServo, wheelServo_2);
     jawServo.write(0);
     handServo.write(0);
-    legServo.write(0);
-    ambientLight = analogRead(ldrPin);
+    legServo.write(30);
+    Serial.println("DINOSAUR IS ALIVE");
+    delay(1000);
+    
+    //calibrate here before evey run
+    for (int i; i<=5 ; i++){
+      Serial.print("LDR TEST: ");
+      Serial.println( i);
+      Serial.print( " AMBIENT LIGHT INTENSITY: ");
+      Serial.println(analogRead(ldrPin));
+      delay(1000);
+    }
+
+    Serial.println("PLEASE CALIBRATE LDR BEFORE USE");
+    //Calibrate here before every run
+    ambientLight = 140;
+    Serial.println("LET THE HUNT BEGIN!");
+    delay(1000);
 }
 
+
 void loop() {
-//    calcDistance();
-//    distanceLed();
-//    buzz(100, 100);
-//    ldr();
-//    if (distance <= 30) {keepDistance(wheelServo, &wheelPos, 5);}
-//    delay(10);
-bite(wheelServo, 0, 180);
+    calcDistance();
+    distanceLed();
+    buzz(1000, 40);
+    //HAND TRACKING
+    if (distance <= 30 && distance != 0) {
+        keepDistance(wheelServo, wheelServo_2, &wheelPos,&wheelPos_2, 15);
+        } else if (distance > 200 && wheelServo.read() >= 180) {
+          reset_wheel(wheelServo,wheelServo_2);
+        } else if (distance < 1) {}  // suppress 0cm noise 
+    ldr();
+    Serial.print("LDR Value: ");
+    Serial.println(analogRead(ldrPin)); // print LDR value for easier debugging
 }
 
 // Calculate the distance using HC-SR04 ultrasonic sensor
@@ -67,52 +89,43 @@ void buzz(int freq, int dur) {
 
 // Detect if object (hand) is present in mouth
 void ldr() {
-    ldrValue = analogRead(ldrPin);
-    if (ldrValue <= ambientLight - 30) {
-        bite(jawServo, 0, 30);
-        bite(handServo, 0, 40);
-        bite(legServo, 0, 60);
-    } else {
-        jawServo.write(0);
-        handServo.write(0);
-        legServo.write(0);
-    }
-    Serial.print("LDR Value: ");
-    Serial.println(ldrValue);
-    Serial.print("Ambient Light Value: ");
-    Serial.println(ambientLight);
+    if (analogRead(ldrPin) < ambientLight-30){
+      Serial.println("HAND DETECTED");
+      bite(legServo, 60, "LEG");
+      bite(handServo, 60, "JAW");
+      bite(jawServo, 60, "HAND");
+      bite(legServo, 30, "LEG");
+      bite(jawServo, 0, "JAW");
+      bite(handServo, 0, "HAND");
+  }
 }
 
 // Jaw and hand servo movement if hand present between mouth
-void bite(Servo servoName, int start, int final) {
-    for (int i=start; i<=final; i++) {
-        servoName.write(i);
-        delay(10);
-    }
-    for (int i=final; i>=start; i--) {
-        servoName.write(i);
-        delay(10);
-    }
-}
-
-// Leg servo movement if hand present between mouth
-void pitch(Servo servoName, int start, int final) {
-    for (int i=final; i>=start; i--) {
-        servoName.write(i);
-        delay(10);
-    }
-    for (int i=start; i<=final; i++) {
-        servoName.write(i);
-        delay(10);
-    }
+void bite(Servo servoName, int end_val, char part_name[]){
+  Serial.print(part_name);
+  Serial.println(" IS MOVING!");
+  servoName.write(end_val);
+  delay(1000);
 }
 
 // Maintain the preset distance with the object
-void keepDistance(Servo servoName, int* posServo, int increment) {
-    if (*posServo > 0 && *posServo < 140) {
-        servoName.write(distance <= presetDistance ? *posServo-=increment : *posServo+=increment);
-    } else if (*posServo == 0 || *posServo == 140) {
-        servoName.write(distance <= presetDistance ? (*posServo == 0 ? 0 : *posServo-=increment) : (*posServo == 0 ? *posServo+=increment : 140));
+void keepDistance(Servo servoName, Servo servoName2,int& posServo, int& posServo_2, int increment) {
+    if (posServo > 0 && posServo < 180) {
+        servoName.write(distance <= presetDistance ? posServo-=increment : posServo+=increment);
+        servoName2.write(distance <= presetDistance ? posServo_2+=increment : posServo_2-=increment);
+    } else if (posServo == 0 || posServo == 180) {
+        servoName.write(distance <= presetDistance ? (posServo == 0 ? 0 : posServo-=increment) : (posServo == 180 ? 180 : posServo+=increment));
+        servoName2.write(distance <= presetDistance ? (posServo_2 == 180 ? 180 : posServo_2+=increment) : (*posServo_2 == 0 ? 0 : posServo_2-=increment));
     }
-    Serial.println(*posServo);
+    Serial.println(posServo);
+    Serial.println(posServo_2);
+}
+
+void reset_wheel(Servo servoName, Servo servoName_2){
+    Serial.println("RESETING WHEEL");
+    for (int i=servoName.read(); i<=90; i++){
+    servoName.write(i);
+    servoName_2.write(180-i);
+    delay(10);
+}
 }
