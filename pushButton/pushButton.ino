59397479ec26426e9d2c0db4770b558e9d34b263
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-12-26 19:11:59 +0800

    pushButton

--- pushButton/pushButton.ino
+++ pushButton/pushButton.ino
@@ -4,13 +4,12 @@ bool buttonState;
 void setup() {
     pinMode(button, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
     buttonState = digitalRead(button);
-    if (buttonState) {
-        digitalWrite(LED_BUILTIN, HIGH);
-    }
+    digitalWrite(LED_BUILTIN, (buttonState ? HIGH : LOW));
     Serial.print("Button state: ");
     Serial.println(buttonState);
     delay(100);
